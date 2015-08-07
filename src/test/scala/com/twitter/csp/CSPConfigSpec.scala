ebff0f8ad21035470887cd361fdec44a61ef72f9
Annie Edmundson <aedmundson@twitter.com>
2015-08-07 10:14:18 -0700

    initial code structure

---
+++ src/test/scala/com/twitter/csp/CSPConfigSpec.scala
@@ -0,0 +1,51 @@
+package com.twitter.appsec.csp
+
+import org.junit.Assert._
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import scala.collection.mutable
+
+@RunWith(classOf[JUnitRunner])
+class CSPConfigTest
+  extends FunSuite {
+
+  test ("a default config should have the correct default values") {
+    val config: CspConfiguration = CspFixtures.defaultConfigEnforce
+    assertEquals(config.reportOnly, false)
+    assertEquals(config.headersMap, mutable.Map(
+      "script-src" -> mutable.Set("'self'"),
+      "style-src" -> mutable.Set("'self'"),
+      "default-src" -> mutable.Set("'self'"),
+      "img-src" -> mutable.Set("'self'"),
+      "media-src" -> mutable.Set("'self'"),
+      "font-src" -> mutable.Set("'self'"),
+      "connect-src" -> mutable.Set("'self'"),
+      "object-src" -> mutable.Set("'self'"),
+      "child-src" -> mutable.Set("'self'"),
+      "frame-ancestors" -> mutable.Set("'self'"),
+      "report-uri" -> mutable.Set("https://twitter.com/i/csp_report"))
+    )
+  }
+
+  test ("a custom config should have the correct specified fields") {
+    val config: CspConfiguration = CspFixtures.defaultConfigReportOnly
+      .withSrcForDirective("script-src", "https:")
+      .withSrcForDirective("style-src", "'self'")
+      .withoutSrcForDirective("script-src", "'self'")
+    assertEquals(config.reportOnly, true)
+    assertEquals(config.headersMap, mutable.Map(
+      "script-src" -> mutable.Set("https:"),
+      "style-src" -> mutable.Set("'self'"),
+      "default-src" -> mutable.Set("'self'"),
+      "img-src" -> mutable.Set("'self'"),
+      "media-src" -> mutable.Set("'self'"),
+      "font-src" -> mutable.Set("'self'"),
+      "connect-src" -> mutable.Set("'self'"),
+      "object-src" -> mutable.Set("'self'"),
+      "child-src" -> mutable.Set("'self'"),
+      "frame-ancestors" -> mutable.Set("'self'"),
+      "report-uri" -> mutable.Set("https://twitter.com/i/csp_report"))
+    )
+  }
+}
---
+++ src/test/scala/com/twitter/csp/CspFixtures.scala
@@ -0,0 +1,57 @@
+package com.twitter.appsec.csp
+
+object CspFixtures {
+
+  val macawName = "macaw-test"
+
+  val chromeUA = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+
+  val safariUA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A"
+
+  val firefoxUA = "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0"
+
+  val ieUA = "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko"
+
+  def defaultConfigEnforce: CspConfiguration = { new EnforcingCspConfiguration()
+    .withSrcForDirective("default-src", "'self'")
+    .withSrcForDirective("script-src", "'self'")
+    .withSrcForDirective("style-src", "'self'")
+    .withSrcForDirective("img-src", "'self'")
+    .withSrcForDirective("media-src", "'self'")
+    .withSrcForDirective("font-src", "'self'")
+    .withSrcForDirective("connect-src", "'self'")
+    .withSrcForDirective("object-src", "'self'")
+    .withSrcForDirective("child-src", "'self'")
+    .withSrcForDirective("frame-ancestors", "'self'")
+    .withSrcForDirective("report-uri", "https://twitter.com/i/csp_report")
+  }
+
+  def defaultConfigEnforceWithNonce: CspConfiguration = { new EnforcingCspConfiguration()
+    .withSrcForDirective("default-src", "'self'")
+    .withSrcForDirective("script-src", "'self'")
+    .withSrcForDirective("style-src", "'self'")
+    .withSrcForDirective("img-src", "'self'")
+    .withSrcForDirective("media-src", "'self'")
+    .withSrcForDirective("font-src", "'self'")
+    .withSrcForDirective("connect-src", "'self'")
+    .withSrcForDirective("object-src", "'self'")
+    .withSrcForDirective("child-src", "'self'")
+    .withSrcForDirective("frame-ancestors", "'self'")
+    .withSrcForDirective("report-uri", "https://twitter.com/i/csp_report")
+    .withScriptNonce()
+  }
+
+  def defaultConfigReportOnly: CspConfiguration = { new ReportOnlyCspConfiguration()
+    .withSrcForDirective("default-src", "'self'")
+    .withSrcForDirective("script-src", "'self'")
+    .withSrcForDirective("style-src", "'self'")
+    .withSrcForDirective("img-src", "'self'")
+    .withSrcForDirective("media-src", "'self'")
+    .withSrcForDirective("font-src", "'self'")
+    .withSrcForDirective("connect-src", "'self'")
+    .withSrcForDirective("object-src", "'self'")
+    .withSrcForDirective("child-src", "'self'")
+    .withSrcForDirective("frame-ancestors", "'self'")
+    .withSrcForDirective("report-uri", "https://twitter.com/i/csp_report")
+  }
+}
